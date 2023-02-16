1ab05bb32904eb93ff43ea7b2f2103cf4ab57060
Hayden <hnightingale01@qub.ac.uk>
2023-02-16 15:32:11 +0000

    initial setup for data utilities

--- src/org/jfree/data/DataUtilitiesTest.java
+++ src/org/jfree/data/DataUtilitiesTest.java
@@ -1,67 +1,63 @@
 package org.jfree.data;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class DataUtilitiesTest {
-	
-	private Values2D values2D;
+import junit.framework.TestCase;
 
+public class DataUtilitiesTest extends TestCase {
+
+	private DefaultKeyedValues2D values2D;
+
+	// Initialises variable for test 
 	@Before
 	public void setUp() throws Exception {
-		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D(); 
-	    values2D = testValues; 
-	    testValues.addValue(1, 0, 0); 
-	    testValues.addValue(4, 1, 0); 
-	}
-	
-	@Test 
-	  public void testValidDataAndColumnColumnTotal() 
-	  { 
-	    assertEquals("Wrong sum returned. It should be 5.0",  
-	        5.0, DataUtilities.calculateColumnTotal(values2D, 0), 0.0000001d); 
-	  } 
-	
-	public void testNullDataColumnTotal()
-	{
-	try
-	{
-	    DataUtilities.calculateColumnTotal(null, 0);
-	    fail("No exception thrown. The expected outcome was: a thrown "
-	    		+ "exception of type: IllegalArgumentException");
-	}
-	catch (Exception e)
-	{
-	    assertTrue("Incorrect exception type thrown",  
-	    e.getClass().equals(IllegalArgumentException.class));
-	}
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
 	}
 
+	// Resets all variables before next test is ran
 	@After
 	public void tearDown() throws Exception {
-		values2D = null; 
+		values2D = null;
 	}
-	
-	//*********************************************************************
-	// Tests for work to be done section of assignment 
-	
-	// Method 1 - Tests for Calculate Column Total 
-	
-	// TC 1 ... 
-	// TC 2 ... 
-	// 
-	// Method 2 - Tests for Calculate Row Total 
-	
-	// Method 3 - Tests for Create Number Array 
-	
-	// Method 4 - Tests for Create Number Array 2D 
-	
-	// Method 5 - Tests for Get Cumulative Percentages 
-	
-	
+
+
+	// ****** Method 1 - Tests for Calculate Column Total *****************
+
+	// Case 1: Column total with all positive values
+	@Test
+	public void testCalculateColumnTotalPositiveValues() {
+		
+		values2D.addValue(1, 0, 0);
+		values2D.addValue(5, 1, 0);
+		values2D.addValue(10, 2, 0);
+		values2D.addValue(15, 3, 0);
+		
+		double colTotal = DataUtilities.calculateColumnTotal(values2D, 0);
+
+		assertEquals(31.0, colTotal);
+	}
+
+	// Case 2: Column total with all negative values
+		@Test
+		public void testCalculateColumnTotalNegativeValues() {			
+			values2D.addValue(-1, 0, 0);
+			values2D.addValue(-5, 1, 0);
+			values2D.addValue(-10, 2, 0);
+			values2D.addValue(-15, 3, 0);
+			
+			double colTotal = DataUtilities.calculateColumnTotal(values2D, 0);
+
+			assertEquals(-31.0, colTotal);
+		}
+	// Method 2 - Tests for Calculate Row Total
+
+	// Method 3 - Tests for Create Number Array
+
+	// Method 4 - Tests for Create Number Array 2D
+
+	// Method 5 - Tests for Get Cumulative Percentages
+
 }
