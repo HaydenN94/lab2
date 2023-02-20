8b2b2a1a6c5964e245c064659e5b20a764c272a2
Fergal Bittles <84090757+fergalbittles@users.noreply.github.com>
2023-02-20 18:42:29 +0000

    add more test cases for Range class

--- src/org/jfree/data/RangeTest.java
+++ src/org/jfree/data/RangeTest.java
@@ -127,7 +127,7 @@ public class RangeTest {
 		Range r1 = new Range(-20, 15);
 		assertEquals("contains: Did not return the expected output", false, r1.contains(41));
 	}
-	
+
 	// ****** Tests for 'expand' method ***********************************
 	
 	// Case 1: expand with a null Range object
@@ -144,9 +144,19 @@ public class RangeTest {
 		}
 	}
 	
-	// Case 2: expand with a Range object of length 0
+	// Case 2: expand with an empty Range object and negative margin values
 	@Test
-	public void testExpandWithEmptyRange() {
+	public void testExpandWithEmptyObjectAndNegativeMargins() {
+		Range r1 = new Range(0, 0);
+		
+		r1 = Range.expand(r1, -0.5, -1.5);
+
+		assertEquals("expand: Did not return the expected output", true, (r1.getLowerBound() == 0 && r1.getUpperBound() == 0));
+	}
+	
+	// Case 3: expand with an empty Range object and positive margin values
+	@Test
+	public void testExpandWithEmptyObjectAndPositiveMargins() {
 		Range r1 = new Range(0, 0);
 		
 		r1 = Range.expand(r1, 0.5, 1.5);
@@ -154,9 +164,29 @@ public class RangeTest {
 		assertEquals("expand: Did not return the expected output", true, (r1.getLowerBound() == 0 && r1.getUpperBound() == 0));
 	}
 	
-	// Case 3: expand with a negative lower margin
+	// Case 4: expand with a populated Range object and zero margin values
 	@Test
-	public void testExpandWithNegativeLowerMargin() {
+	public void testExpandWithPopulatedObjectAndZeroMargins() {
+		Range r1 = new Range(2, 6);
+		
+		r1 = Range.expand(r1, 0.0, 0.0);
+
+		assertEquals("expand: Did not return the expected output", true, (r1.getLowerBound() == 2 && r1.getUpperBound() == 6));
+	}
+	
+	// Case 5: expand with a populated Range object and a negative upper margin
+	@Test
+	public void testExpandWithPopulatedObjectAndNegativeUpperMargin() {
+		Range r1 = new Range(2, 6);
+		
+		r1 = Range.expand(r1, 0.5, -0.25);
+
+		assertEquals("expand: Did not return the expected output", true, (r1.getLowerBound() == 0 && r1.getUpperBound() == 5));
+	}
+	
+	// Case 6: expand with a populated Range object and negative lower margin
+	@Test
+	public void testExpandWithPopulatedObjectAndNegativeLowerMargin() {
 		Range r1 = new Range(2, 6);
 		
 		r1 = Range.expand(r1, -0.25, 0.5);
@@ -164,18 +194,108 @@ public class RangeTest {
 		assertEquals("expand: Did not return the expected output", true, (r1.getLowerBound() == 3 && r1.getUpperBound() == 8));
 	}
 	
-	// ****** Tests for 'expandToInclude' method **************************
-	
-	// Case 1: expandToInclude with a null Range object
+	// Case 7: expand with a populated Range object and positive margin values
 	@Test
-	public void testExpandToIncludeWithNullObject() {
-		Range r1 = null;
+	public void testExpandWithPopulatedObjectAndPositiveMarginValues() {
+		Range r1 = new Range(2, 6);
 		
-		r1 = Range.expandToInclude(r1, 5.0);
-			
-		assertTrue("expandToInclude: Did not return the expected output", r1.contains(5.0));
+		r1 = Range.expand(r1, 0.25, 0.5);
+
+		assertEquals("expand: Did not return the expected output", true, (r1.getLowerBound() == 1 && r1.getUpperBound() == 8));
 	}
 	
+	// ****** Tests for 'expandToInclude' method **************************
+	
+	// Case 1: expandToInclude with a null Range object and a negative value
+	@Test
+	public void testExpandToIncludeWithNullObjectAndNegativeValue() {
+		Range r1 = null;
+		
+		r1 = Range.expandToInclude(r1, -5.0);
+			
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(-5.0));
+	}
+	
+	// Case 2: expandToInclude with a null Range object and a zero value
+	@Test
+	public void testExpandToIncludeWithNullObjectAndZeroValue() {
+		Range r1 = null;
+		
+		r1 = Range.expandToInclude(r1, 0.0);
+			
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(0.0));
+	}
+	
+	// Case 3: expandToInclude with a null Range object and a positive value
+	@Test
+	public void testExpandToIncludeWithNullObjectAndPositiveValue() {
+		Range r1 = null;
+		
+		r1 = Range.expandToInclude(r1, 8.0);
+			
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(8.0));
+	}
+	
+	// Case 4: expandToInclude with an empty Range object and a negative value
+	@Test
+	public void testExpandToIncludeWithEmptyObjectAndNegativeValue() {
+		Range r1 = new Range(0, 0);
+		
+		r1 = Range.expandToInclude(r1, -5.0);
+			
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(-5.0));
+	}
+	
+	// Case 5: expandToInclude with an empty Range object and a zero value
+	@Test
+	public void testExpandToIncludeWithEmptyObjectAndZeroValue() {
+		Range r1 = new Range(0, 0);
+		
+		r1 = Range.expandToInclude(r1, 0.0);
+		
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(0.0));
+	}
+	
+	// Case 6: expandToInclude with an empty Range object and a positive value
+	@Test
+	public void testExpandToIncludeWithEmptyObjectAndPositiveValue() {
+		Range r1 = new Range(0, 0);
+		
+		r1 = Range.expandToInclude(r1, 8.0);
+		
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(8.0));
+	}
+	
+	// Case 7: expandToInclude with a populated Range object and a negative value
+	@Test
+	public void testExpandToIncludeWithPopulatedObjectAndNegativeValue() {
+		Range r1 = new Range(-3, 3);
+		
+		r1 = Range.expandToInclude(r1, -5.0);
+		
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(-5.0));
+	}
+	
+	// Case 8: expandToInclude with a populated Range object and a zero value
+	@Test
+	public void testExpandToIncludeWithPopulatedObjectAndZeroValue() {
+		Range r1 = new Range(-15, -5);
+		
+		r1 = Range.expandToInclude(r1, 0.0);
+		
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(0.0));
+	}
+	
+	// Case 9: expandToInclude with a populated Range object and a positive value
+	@Test
+	public void testExpandToIncludeWithPopulatedObjectAndPositiveValue() {
+		Range r1 = new Range(-3, 3);
+		
+		r1 = Range.expandToInclude(r1, 8.0);
+		
+		assertTrue("expandToInclude: Did not return the expected output", r1.contains(8.0));
+	}
+
 	// ****** Tests for 'combine' method **********************************
 	
 	// Case 1: combine with two null Range objects
@@ -187,6 +307,83 @@ public class RangeTest {
 		Range r3 = Range.combine(r1, r2);
 			
 		assertEquals("expandToInclude: Did not return the expected output", null, r3);
-	}	
+	}
+	
+	// Case 2: combine with one null Range object and one empty Range object
+	@Test
+	public void testCombineWithOneNullAndOneEmptyObject() {
+		Range r1 = new Range(0, 0);
+		Range r2 = null;
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.getLength() == 0 && r3.contains(0));
+	}
+	
+	// Case 3: combine with one null Range object
+	@Test
+	public void testCombineWithOneNullObject() {
+		Range r1 = new Range(10, 15);
+		Range r2 = null;
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.getLength() == 5 && r3.contains(10) && r3.contains(15));
+	}
+	
+	// Case 4: combine with two empty Range objects
+	@Test
+	public void testCombineWithTwoEmptyObjects() {
+		Range r1 = new Range(0, 0);
+		Range r2 = new Range(0, 0);
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.getLength() == 0 && r3.contains(0));
+	}
+	
+	// Case 5: combine with one empty Range object and one populated Range object
+	@Test
+	public void testCombineWithOneEmptyAndOnePopulatedObject() {
+		Range r1 = new Range(2, 6);
+		Range r2 = new Range(0, 0);
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.contains(2) && r3.contains(6));
+	}
+	
+	// Case 6: combine with one null Range object and one negative value Range object
+	@Test
+	public void testCombineWithOneNullAndOneNegativeValueObject() {
+		Range r1 = null;
+		Range r2 = new Range(-15, -10);
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.contains(-15) && r3.contains(-10));
+	}
+	
+	// Case 7: combine with one empty Range object and one negative value Range object
+	@Test
+	public void testCombineWithOneEmptyAndOneNegativeValueObject() {
+		Range r1 = new Range(0, 0);
+		Range r2 = new Range(-15, -10);
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.contains(-15) && r3.contains(-10));
+	}
+	
+	// Case 8: combine with one positive value Range object and one negative value Range object
+	@Test
+	public void testCombineWithOnePositiveValueAndOneNegativeValueObject() {
+		Range r1 = new Range(2, 6);
+		Range r2 = new Range(-15, -10);
+		
+		Range r3 = Range.combine(r1, r2);
+			
+		assertTrue("combine: Did not return the expected output", r3.contains(-15) && r3.contains(6));
+	}
 
 }
